4d71c5105853b8369490d7157bba0b6edb52a47d
PatiMohit <patidarmohit.iit@gmail.com>
2020-11-27 18:26:04 +0530

    HashSet concurrency affect on adding only

---
+++ src/com/multithreading/practice/ConcurrencyOnHashSet.java
@@ -0,0 +1,31 @@
+package com.multithreading.practice;
+
+import java.util.HashSet;
+
+public class ConcurrencyOnHashSet {
+
+    private static HashSet<String> hashSet = new HashSet<>();
+
+
+    public static void main(String args[]) throws InterruptedException {
+
+        Thread t1 = new Thread(r1);
+        Thread t2 = new Thread(r1);
+        t1.start();
+        t2.start();
+        Thread.sleep(15000);
+
+        hashSet.forEach(System.out::println);
+
+    }
+
+    static Runnable r1 = new Runnable() {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                hashSet.add("Thread : "+Thread.currentThread().getName()+ " i : " +i);
+            }
+        }
+    };
+
+}
