801376c1b7ce58ddee6f759a74531c976aa9e38d
PatiMohit <patidarmohit.iit@gmail.com>
2021-01-24 21:53:58 +0530

    Threadpool

--- src/com/multithreading/concept/threadpool/ThreadPoolExample.java
+++ src/com/multithreading/concept/threadpool/ThreadPoolExample.java
@@ -1,4 +1,23 @@
 package com.multithreading.concept.threadpool;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadPoolExample {
+    public static void main(String args[]){
+        Executor executor= Executors.newFixedThreadPool(3);
+
+        for(int i=0;i<5;i++) {
+            ((ExecutorService) executor).submit(new Task());
+        }
+    }
+
+
+}
+class Task implements Runnable{
+
+    public void run(){
+        System.out.println("task is running by thread : "+ Thread.currentThread().getName());
+    }
 }
